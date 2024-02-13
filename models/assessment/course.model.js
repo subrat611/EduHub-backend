c0d10a69e9d52df037734e7926855ce8f552833a
Subrat kumar jena <subratj611@gmail.com>
2024-02-14 00:42:40 +0530

    fix: typo fix

--- models/assessment/course.model.js
+++ models/assessment/course.model.js
@@ -1,37 +1,41 @@
 const Joi = require("joi");
 const { mongoose, Schema } = require("mongoose");
 
-const courseSchema = new Schema({
-  course_creator: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
+const courseSchema = new Schema(
+  {
+    course_creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    total_videos: {
+      type: Number,
+    },
+    total_enrolled: {
+      type: Number,
+    },
+    difficulty: {
+      type: String,
+    },
   },
-  title: {
-    type: String,
-    require: true,
-  },
-  subtitle: {
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  category: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  total_videos: {
-    type: Number,
-  },
-  total_enrolled: {
-    type: Number,
-  },
-  difficulty: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const Course = mongoose.model("course", courseSchema);
 
